6e2d014c29ae80c1d32b77516d5b820912821bc7
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-13 08:58:41 +0400

    Update Dashboard.tsx

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/Components/ui/Button';
 import { 
   Users, 
   Briefcase, 
