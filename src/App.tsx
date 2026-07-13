b5b29c39d4d590ec4497a01edabb256ceba40da4
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-13 10:02:23 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
 import { Sidebar } from './components/layout/sidebar';
 import { MobileNav } from './components/layout/MobileNav';
+
 import { Dashboard } from './Pages/Dashboard';
+import { Cases } from './pages/Cases';
+import { Tasks } from './pages/Tasks';
+import { CalendarPage } from './pages/CalendarPage';
+import { More } from './pages/More';
 
 function App() {
   return (
     <BrowserRouter basename="/SHAB-/">
       <div className="min-h-screen bg-gray-50">
         <Sidebar />
-        <div className="lg:ml-64 pb-20 lg:pb-0">
+
+        <main className="pb-24 lg:ml-64 lg:pb-0">
           <Routes>
             <Route path="/" element={<Dashboard />} />
+            <Route path="/cases" element={<Cases />} />
+            <Route path="/tasks" element={<Tasks />} />
+            <Route path="/calendar" element={<CalendarPage />} />
+            <Route path="/more" element={<More />} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-        </div>
+        </main>
+
         <MobileNav />
       </div>
     </BrowserRouter>
