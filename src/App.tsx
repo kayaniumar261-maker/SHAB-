716c1048f86d6814e6bcca7f97fe6d56b183d554
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-13 05:27:20 +0400

    Initialize App component with routing and layout

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Sidebar } from './components/layout/Sidebar';
+import { MobileNav } from './components/layout/MobileNav';
+import { Dashboard } from './pages/Dashboard';
+
+function App() {
+  return (
+    <BrowserRouter basename="/SHAB-/">
+      <div className="min-h-screen bg-gray-50">
+        <Sidebar />
+        <div className="lg:ml-64 pb-20 lg:pb-0">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+        <MobileNav />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
