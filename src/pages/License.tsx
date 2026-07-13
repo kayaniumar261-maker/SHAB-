e256bc779a90a8a7616f52e5b6cd96ce290a8240
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-13 09:00:58 +0400

    Update License.tsx

--- src/pages/License.tsx
+++ src/pages/License.tsx
@@ -8,7 +8,7 @@
  */
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/Components/ui/Button';
 import { 
   Scale, 
   Shield, 
