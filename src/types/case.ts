32dd3abb6b0be7c6671af591e495bba410d2a2df
kayaniumar261-maker <kayaniumar261@gmail.com>
2026-07-15 11:52:59 +0400

    Add LegalCase interface to case.ts

---
+++ src/types/case.ts
@@ -0,0 +1,34 @@
+import {
+  BaseRecord,
+  Priority,
+  RecordStatus,
+} from './common';
+
+export interface LegalCase
+  extends BaseRecord {
+  title: string;
+
+  clientReference: string;
+
+  clientName: string;
+
+  court: string;
+
+  caseNumber: string;
+
+  category: string;
+
+  assignedLawyer: string;
+
+  opponent: string;
+
+  filingDate: string;
+
+  nextHearing?: string;
+
+  priority: Priority;
+
+  status: RecordStatus;
+
+  notes: string;
+}
